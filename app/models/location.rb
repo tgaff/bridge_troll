1a01526923057bdd99c6c7676ec5d88702156552
Travis Gaff <tgaff@alumni.nd.edu>
2013-03-28 20:25:59 -0700

    Reconfigure gmap4rails to improve unfriendly error message   'gmap4rails_address address invalid' is not a user friendly error   improved to 'address not valid'
      The way this is setup in gmap4rails is not ideal, but its better
      than it was.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,9 +1,10 @@
 class Location < ActiveRecord::Base
   has_many :events
   validates_presence_of :name, :address_1, :city
-  acts_as_gmappable(process_geocoding: !Rails.env.test?)
+  acts_as_gmappable(process_geocoding: !Rails.env.test?, msg: 'not valid', address: 'address')
 
-  def gmaps4rails_address
-    "#{self.address_1}, #{self.city}, #{self.state}, #{self.zip}" 
+  def address
+    "#{self.address_1}, #{self.city}, #{self.state}, #{self.zip}"
   end
+
 end
